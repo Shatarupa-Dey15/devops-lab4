2709fe49bcaec70bb65aa9b0d7ee4f6630a0b9e0
Shatarupa-Dey15 <2021wb86288@wilp.bits-pilani.ac.in>
2025-07-06 03:53:30 +0530

    Updated App.java locally

--- src/main/java/com/devops/lab/App.java
+++ src/main/java/com/devops/lab/App.java
@@ -7,5 +7,6 @@ public class App
         System.out.println( "Hello From Continuous Integration Triggers!" );
         System.out.println("Devops Lab 4 completes if the first Line of Output shows Started by SCM");
         System.out.println("Hello From CI");
+        System.out.println(2 +2);
     }
 }
